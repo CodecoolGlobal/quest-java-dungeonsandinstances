8ab0a56d8c2c7f984239820177a5e9fecc85959c
Katalin Nemeth <zeoramail@gmail.com>
2019-10-10 11:06:46 +0200

    CellyType canMoveToNExtCell method added

--- src/main/java/com/codecool/quest/logic/CellType.java
+++ src/main/java/com/codecool/quest/logic/CellType.java
@@ -14,5 +14,14 @@ public enum CellType {
     public String getTileName() {
         return tileName;
     }
-    
+
+
+    public boolean canMoveToNExtCell() {
+
+        for (CellType tile : CellType.values()) {
+            //System.out.println(tile);
+            if (tile.equals(FLOOR)) return true;
+            else {return false;}
+        }
+    }
 }
